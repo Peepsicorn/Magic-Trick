e0b58e72a7db9c13f90b0ede531d947147087443
eweslikemath <piper.d.mathews@gmail.com>
2020-11-28 20:59:06 +0000

    added cards

--- is-it.py
+++ is-it.py
@@ -5,4 +5,40 @@ class Card:
     self.num = num
 
 class Stack:
+ def __init__(self, items):
+   self.items = items
+
+class Deck:
+  5hearts = Card ("hearts", 5)
+  6hearts = Card ("hearts", 6)
+  7hearts = Card ("hearts", 7)
+  8hearts = Card ("hearts", 8)
+  9hearts = Card ("hearts", 9)
+  10hearts = Card ("hearts", 10)
+
+  5spades = Card ("spades", 5)
+  6spades = Card ("spades", 6)
+  7spades = Card ("spades", 7)
+  8spades = Card ("spades", 8)
+  9spades = Card ("spades", 9)
+  10spades = Card ("spades", 10)
+
+  5clubs = Card ("clubs", 5)
+  6clubs = Card ("clubs", 6)
+  7clubs = Card ("clubs", 7)
+  8clubs = Card ("clubs", 8)
+  9clubs = Card ("clubs", 9)
+  10clubs = Card ("clubs", 10)
+
+  4diamonds = Card ("diamonds", 4)
+  5diamonds = Card ("diamonds", 5)
+  6diamonds = Card ("diamonds", 6)
+  7diamonds = Card ("diamonds", 7)
+  8diamonds = Card ("diamonds", 8)
+  9diamonds = Card ("diamonds", 9)
+  10diamonds = Card ("diamonds", 10)
+
+
+
+  
   
